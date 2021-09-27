a303e2d903908dbe02043aab65f2180267639919
mikeisfake <mikescooper@icloud.com>
2021-09-27 13:33:01 -0400

    add custom colo and heights

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,20 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navy: "#030d2e",
+      },
+      height: {
+        "50-screen": "50vh",
+        "60-screen": "60vh",
+        "70-screen": "70vh",
+        "80-screen": "80vh",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
